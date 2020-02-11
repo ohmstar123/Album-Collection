d2f03e50355bc8a8515b9f4090a71d2697ffccc0
Ohm Patel <ohmpate114@ed.amdsb.ca>
2020-02-11 13:32:21 -0500

    Add project files.

---
+++ Album Collection/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Album_Collection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.originalLabel = new System.Windows.Forms.Label();
+            this.sortedLabel = new System.Windows.Forms.Label();
+            this.addButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Information";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(179, 33);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(245, 26);
+            this.inputBox.TabIndex = 1;
+            // 
+            // originalLabel
+            // 
+            this.originalLabel.Location = new System.Drawing.Point(26, 106);
+            this.originalLabel.Name = "originalLabel";
+            this.originalLabel.Size = new System.Drawing.Size(364, 318);
+            this.originalLabel.TabIndex = 2;
+            this.originalLabel.Text = "Original Order";
+            // 
+            // sortedLabel
+            // 
+            this.sortedLabel.Location = new System.Drawing.Point(396, 106);
+            this.sortedLabel.Name = "sortedLabel";
+            this.sortedLabel.Size = new System.Drawing.Size(364, 318);
+            this.sortedLabel.TabIndex = 3;
+            this.sortedLabel.Text = "Sorted Order";
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(509, 36);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(98, 41);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(662, 36);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(98, 41);
+            this.removeButton.TabIndex = 5;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.sortedLabel);
+            this.Controls.Add(this.originalLabel);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Label originalLabel;
+        private System.Windows.Forms.Label sortedLabel;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button removeButton;
+    }
+}
+
---
+++ Album Collection/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Album_Collection
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> addedNames = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string names = inputBox.Text;
+
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
