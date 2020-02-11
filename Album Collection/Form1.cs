27a3b6dededb70ea5e65cb48a285dc596f29dbbd
Ohm Patel <ohmpate114@ed.amdsb.ca>
2020-02-11 18:16:56 -0500

    stuck

--- Album Collection/Form1.cs
+++ Album Collection/Form1.cs
@@ -13,18 +13,33 @@ namespace Album_Collection
     public partial class Form1 : Form
     {
 
-        List<string> addedNames = new List<string>();
+       
+
+        static public List<string> addedNames = new List<string>();
 
         public Form1()
         {
             InitializeComponent();
 
+            
+
+            originalLabel.Text += "\n" + "\n";
+            sortedLabel.Text += "\n" + "\n";
+            
 
         }
 
         private void addButton_Click(object sender, EventArgs e)
         {
             string names = inputBox.Text;
+            addedNames.Add(names);
+
+            for (int i = 0; i < addedNames.Count(); i++)
+            {
+                originalLabel.Text = "\n" + addedNames[i] ;
+            }
+
+
 
         }
 
