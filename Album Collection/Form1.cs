32914149f428975dfd674a9e4ad35fb12dd06963
Ohm Patel <ohmpate114@ed.amdsb.ca>
2020-02-12 13:28:16 -0500

    Need help with the remove button

--- Album Collection/Form1.cs
+++ Album Collection/Form1.cs
@@ -15,7 +15,8 @@ namespace Album_Collection
 
        
 
-        static public List<string> addedNames = new List<string>();
+        List<string> addedNames = new List<string>();
+        List<string> sortedNames = new List<string>();
 
         public Form1()
         {
@@ -25,7 +26,9 @@ namespace Album_Collection
 
             originalLabel.Text += "\n" + "\n";
             sortedLabel.Text += "\n" + "\n";
+
             
+
 
         }
 
@@ -33,10 +36,23 @@ namespace Album_Collection
         {
             string names = inputBox.Text;
             addedNames.Add(names);
+            sortedNames.Add(names);
+           
+
+            originalLabel.Text = "Original Order" + "\n" + "\n";
+            sortedLabel.Text = "Sorted Order" + "\n" + "\n";
+
 
             for (int i = 0; i < addedNames.Count(); i++)
             {
-                originalLabel.Text = "\n" + addedNames[i] ;
+                originalLabel.Text += addedNames[i] + "\n";
+            }
+
+            sortedNames.Sort();
+
+            for (int i = 0; i < sortedNames.Count(); i++)
+            {
+                sortedLabel.Text += sortedNames[i] + "\n";
             }
 
 
@@ -45,6 +61,30 @@ namespace Album_Collection
 
         private void removeButton_Click(object sender, EventArgs e)
         {
+            string names = inputBox.Text;
+            addedNames.Add(names);
+            sortedNames.Add(names);
+
+            originalLabel.Text = "Original Order" + "\n" + "\n";
+            sortedLabel.Text = "Sorted Order" + "\n" + "\n";
+
+            for (int i = 0; i < sortedNames.Count(); i++)
+            {
+                if (names == sortedNames[i])
+                {
+                    sortedNames.RemoveAt(i);
+                }
+            }
+
+            sortedNames.Sort();
+
+            for (int i = 0; i < sortedNames.Count(); i++)
+            {
+                sortedLabel.Text += sortedNames[i] + "\n";
+            }
+
+
+
 
         }
     }
